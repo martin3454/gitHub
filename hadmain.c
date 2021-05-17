a88e0766190f7826f3e5c59d6c520d78453bc138
martin3454 <81327855+martin3454@users.noreply.github.com>
2021-05-17 17:26:09 +0200

    Update hadmain.c

--- hadmain.c
+++ hadmain.c
@@ -1,32 +1,3 @@
-/*
- * Copyright (c) 2015, Freescale Semiconductor, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of Freescale Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
 #include <header.h>
 #include <stdio.h>
@@ -67,7 +38,6 @@ Node* cre(Cord cor){
 
 	return pt;
 }
-
 
 Node* listik(Node *pt, Node **tail, Cord cor){
 
@@ -207,11 +177,6 @@ int main(void)
 
 		char tlacitko;
 
-
-
-
-
-
 		while(1){
 
 				if(start){
@@ -235,8 +200,6 @@ int main(void)
 
 
 				case 'd':{
-
-
 					start=0;
 					rozdil=hlavaX-ocasX;
 					krok=1;
@@ -394,22 +357,6 @@ int main(void)
 
 				}
 			}*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
